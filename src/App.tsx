1a20533099a2a4a63dc25d531fca1eb5a89a317a
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2022-03-01 12:20:15 -0300

    feat: criando estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
-  return <h1>The Money</h1>;
+  return (
+    <>
+      <h1>The Money</h1>
+      <GlobalStyle />
+    </>
+  );
 }
